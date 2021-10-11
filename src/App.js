696dca05752eb8301ac959c7dcc436275d37cd1c
Carururu <cdvsalanga@gmail.com>
2021-10-11 19:47:05 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Home } from "./pages";
+
+// Font
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
